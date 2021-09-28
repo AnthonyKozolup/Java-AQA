37993d2038577c7826643b19cd12de4d663060f1
AnthonyKozolup <a.kozolup@andersenlab.com>
2021-09-28 17:15:48 +0300

    Third Lesson

--- JavaLessons/src/main/java/Lesson3/Staff.java
+++ JavaLessons/src/main/java/Lesson3/Staff.java
@@ -1,11 +1,31 @@
 package Lesson3;
-
-public class Staff {
- // 1. Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст.
+// 1. Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст.
 // 2. Конструктор класса должен заполнять эти поля при создании объекта.
 // 3. Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль.
-// 4. Создать массив из 5 сотрудников.
-// 5. С помощью цикла вывести информацию только о сотрудниках старше 40 лет.
 
+public class Staff {
 
+  private String name;
+  private String position;
+  private String email;
+  private String phoneNo;
+  private int wage;
+  private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+   public Staff (String name, String position, String email, String phoneNo, int wage, int age){
+       this.name=name;
+       this.position=position;
+       this.email=email;
+       this.phoneNo=phoneNo;
+       this.wage=wage;
+       this.age=age;
+   }
+    public void print() {
+        System.out.println("Сотрудник: " + name + ", должность: " + position + ", почта: " + email + ", тел. " + phoneNo
+                +", з/п: " + wage + ", возраст: " + age);
+    }
 }
